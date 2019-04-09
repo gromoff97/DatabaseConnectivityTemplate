21ec8f8d42092c0dc857794104d67e11b6557f4c
gromoff97 <gromoff97@mail.ru>
2019-04-09 18:42:46 +0300

    constant-values were moved to just created static constant variables

--- src/myconn/DotaPickDBManager.java
+++ src/myconn/DotaPickDBManager.java
@@ -2,10 +2,12 @@ package myconn;
 
 public class DotaPickDBManager extends DatabaseAdapter {
 
+    private static final String jdbUrl = "jdbc:postgresql://localhost:5432/DotaPickDB";
+    private static final String username = "Anthony";
+    private static final String password = "";
+
     public DotaPickDBManager() {
-        super("jdbc:postgresql://localhost:5432/DotaPickDB",
-                "Anthony",
-                "");
+        super(jdbUrl, username, password);
     }
 
 }
