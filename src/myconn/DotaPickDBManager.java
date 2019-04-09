e373682c0a970863d23d18b3a1d6399778a97168
gromoff97 <gromoff97@mail.ru>
2019-04-09 19:30:23 +0300

    createTables was implemented (just creates table with two columns : "id" and "hero_name")

--- src/myconn/DotaPickDBManager.java
+++ src/myconn/DotaPickDBManager.java
@@ -1,5 +1,7 @@
 package myconn;
 
+import java.sql.SQLException;
+
 public class DotaPickDBManager extends DatabaseAdapter {
 
     private static final String jdbUrl = "jdbc:postgresql://localhost:5432/DotaPickDB";
@@ -8,6 +10,21 @@ public class DotaPickDBManager extends DatabaseAdapter {
 
     public DotaPickDBManager() {
         super(jdbUrl, username, password);
+    }
+
+    public void createTables(){
+        try {
+            this.stmnt = this.conn.createStatement();
+
+            String query = "CREATE TABLE IF NOT EXISTS HEROES (" +
+                    "ID SERIAL PRIMARY KEY," +
+                    "HERO_NAME VARCHAR(40) NOT NULL UNIQUE);";
+
+            this.stmnt.execute(query);
+            this.stmnt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
